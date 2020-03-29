6d339e5ed995040b309d4e04404463cb696630b7
Carley Tripp <carley.tripp@outlook.com>
2020-03-29 01:16:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
-    .then(json => renderBooks(json))
-    .then(return);
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
